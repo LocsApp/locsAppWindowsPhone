0e3927e7ef576bb23aa09d1b6fffb49d4d20dc5b
Maxime Duboy <maxime.duboy@epitech.eu>
2015-10-09 09:45:07 +0200

    Init Project windows phone, Models + Basic View

---
+++ Locsapp_Win_Phone/Locsapp_Win_Phone/Models/LoginDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locsapp_Win_Phone.Models
+{
+    class LoginDetails
+    {
+        public string email { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string username { get; set; }
+        public string password1 { get; set; }
+        public string password2 { get; set; }
+        public string birthday { get; set; }
+        public string phone { get; set; }
+        public string living_adress { get; set; }
+        public string biling_adress { get; set; }
+        public string logo_url { get; set; }
+        public string is_active { get; set; }
+    }
+}
---
+++ Locsapp_Win_Phone/Locsapp_Win_Phone/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Locsapp_Win_Phone.Models;
+
+namespace Locsapp_Win_Phone.ViewModels
+{
+    class MainViewModel
+    {
+        const string ApiUrl = @"http://127.0.0.1:8000/";
+    }
+}
