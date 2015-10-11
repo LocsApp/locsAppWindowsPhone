d899ecf0a20946183a3ab8e2ff13cde46f499ca7
Maxime Duboy <maxime.duboy@epitech.eu>
2015-10-11 07:49:38 +0200

    Add modify views

---
+++ Locsapp_Win_Phone/Locsapp_Win_Phone/Views/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Locsapp_Win_Phone.Models;
+
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Locsapp_Win_Phone
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private const string URL = "http://127.0.0.1:8000";
+        public MainPage()
+        {
+            this.InitializeComponent();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+        }
+    }
+}
