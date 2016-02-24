8293497158e60b98084520f427d2497a5d49bf27
Maxime Duboy <maxime.duboy@epitech.eu>
2016-02-24 15:45:01 +0800

    Desgin Profile + Begin Design Edit Profile

---
+++ Locsapp_Win_Phone/Locsapp_Win_Phone/Views/Profil_Design.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Collections.Specialized;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Net;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Locsapp_Win_Phone.Models;
+using Locsapp_Win_Phone.ViewModels;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace Locsapp_Win_Phone
+{
+    public sealed partial class Profil_Design : Page
+    {
+
+        public Profil_Design()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
