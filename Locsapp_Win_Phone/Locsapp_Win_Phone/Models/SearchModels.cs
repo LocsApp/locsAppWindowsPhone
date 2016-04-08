3a272f9369ee1dad603e84a4ca8991c063c74e79
Maxime Duboy <maxime.duboy@epitech.eu>
2016-04-08 12:22:13 +0800

    Add views for search template + buildsearch class

---
+++ Locsapp_Win_Phone/Locsapp_Win_Phone/Models/SearchModels.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Locsapp_Win_Phone.Models;
+using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Net;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Threading;
+using Newtonsoft;
+using Newtonsoft.Json;
+namespace Locsapp_Win_Phone.Models
+{
+    class SearchModels
+    {
+    }
+
+    class BuildSearchList
+    {
+        [JsonProperty("field_search")]
+        public string FieldSearch { get; set; }
+        [JsonProperty("check_search")]
+        public bool CheckSearch { get; set; }
+    }
+
+}
---
+++ Locsapp_Win_Phone/Locsapp_Win_Phone/Views/State.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Collections.Specialized;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Net;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Locsapp_Win_Phone.Models;
+using Locsapp_Win_Phone.ViewModels;
+using Newtonsoft;
+using Newtonsoft.Json;
+using Windows.UI.Xaml.Media.Imaging;
+using System.Collections.ObjectModel;
+
+namespace Locsapp_Win_Phone
+{
+    public sealed partial class State : Page
+    {
+        public State()
+        {
+            this.InitializeComponent();
+            Debug.WriteLine("Salut les amis");
+            ObservableCollection<BuildSearchList> lists = new ObservableCollection<BuildSearchList>();
+            lists.Add(new BuildSearchList { CheckSearch = true, FieldSearch = "Well-Worn" });
+            lists.Add(new BuildSearchList { CheckSearch = false, FieldSearch = "A Little Used" });
+            lists.Add(new BuildSearchList { CheckSearch = false, FieldSearch = "Minimal Wear" });
+            lists.Add(new BuildSearchList { CheckSearch = true, FieldSearch = "Factory New" });
+            MyList.ItemsSource = lists;
+            Debug.WriteLine("Salut les amis 2");
+        }
+    }
+}
