2cb9496bf8fa728d116df615e11fd1d4ccd543ec
Maxime Duboy <maxime.duboy@epitech.eu>
2016-04-12 14:39:12 +0800

    Get all billing/living Address

--- Locsapp_Win_Phone/Locsapp_Win_Phone/Views/ProfilDesign.xaml.cs
+++ Locsapp_Win_Phone/Locsapp_Win_Phone/Views/ProfilDesign.xaml.cs
@@ -60,13 +60,19 @@ namespace Locsapp_Win_Phone
         private void InitAddress(List<List<string>> Living, List<List<string>> Billing)
         {
             List<AddressDisplay> LivingList = new List<AddressDisplay>();
+            List<AddressDisplay> BillingList = new List<AddressDisplay>();
             foreach (List<string> Field in Living)
             {
                 var results = JsonConvert.DeserializeObject<AddressDisplay>(Field[1]);
                 results.Alias = Field[0];
                 LivingList.Add(results);
             }
-            Debug.WriteLine("La list des data est : " + LivingList[0].Alias + " LALAL : " + LivingList[0].City);
+            foreach (List<string> Field in Billing)
+            {
+                var results = JsonConvert.DeserializeObject<AddressDisplay>(Field[1]);
+                results.Alias = Field[0];
+                BillingList.Add(results);
+            }
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
